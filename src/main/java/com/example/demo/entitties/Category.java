e1692a67afcbfc602939bea6bd5de092f64f6a5d
Luisa <luisacavalheiro@gmail.com>
2022-09-16 12:18:41 -0300

    Audicting Data

--- src/main/java/com/example/demo/entitties/Category.java
+++ src/main/java/com/example/demo/entitties/Category.java
@@ -1,11 +1,15 @@
 package com.example.demo.entitties;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +21,30 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
+    }
 
     public Category() {
     }
@@ -30,13 +58,9 @@ public class Category implements Serializable {
         return id;
     }
 
-    
-
     public String getName() {
         return name;
     }
-
-
 
     public void setId(Long id) {
         this.id = id;
@@ -71,5 +95,4 @@ public class Category implements Serializable {
         return true;
     }
 
-  
 }
